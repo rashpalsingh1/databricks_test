91554212da32e967f79f6cbcbce755a7de0cbb69
rashpalsingh1 <rashpalsingh1>
2023-06-23 10:46:15 +0000

    continued and completed questions 1-5

--- Week1/Day2/Coffee.py
+++ Week1/Day2/Coffee.py
@@ -7,7 +7,7 @@
 
 #
 
-coffee= spark.read.csv("file:/Workspace/Repos/rashpal.singh@nrscotland.onmicrosoft.com/databricks_test/Week1/Day2/starbucks_drinkMenu_expanded.csv", header = True, inferSchema= True)
+coffee= spark.read.csv("file:/Workspace/Repos/rashpal.singh@nrscotland.onmicrosoft.com/databricks_test2/Week1/Day2/starbucks_drinkMenu_expanded.csv", header = True, inferSchema= True)
 
 # COMMAND ----------
 
@@ -33,19 +33,33 @@ coffee_subset = coffee.select("Beverage_category", "Beverage", "Beverage_prep", 
 
 # COMMAND ----------
 
-from pyspark.sql.functions import isNull
-
-coffee_subset.select("Calories").isNull().count()
-
-# COMMAND ----------
-
-coffee_subset.select([F.count(F.when(F.isnan(c) | F.col(c).F.isNull(), c)).alias(c) for c in coffee_subset.columns]).show()
+#Check for Null values
+from pyspark.sql.functions import col
+coffee_subset.filter(col("Calories").isNull()).count()
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC Question 3
 # MAGIC Create a new variable (column) called calorie_diff, which stores the difference between 135 calories (135 calories = 10 cubes of sugar!) and the calories in each drink. (hint: you’ll want to subtract the calories from 135 to see which drink have more or less than 10 cups of sugar).
+
+# COMMAND ----------
+
+#covert column names to lowercase
+from pyspark.sql.functions import lower
+coffee_lower = coffee_subset.toDF(*[c.lower() for c in coffee_subset.columns])
+
+# COMMAND ----------
+
+display(coffee_lower)
+
+# COMMAND ----------
+
+coffee_lower = coffee_lower.withColumn("calorie_diff", 135 - col("calories"))
+
+# COMMAND ----------
+
+display(coffee_lower)
 
 # COMMAND ----------
 
@@ -56,9 +70,18 @@ coffee_subset.select([F.count(F.when(F.isnan(c) | F.col(c).F.isNull(), c)).alias
 
 # COMMAND ----------
 
+from pyspark.sql.functions import mean
+coffee_lower.groupBy("beverage_category").agg(mean("calories")).sort("avg(calories)", ascending = False).show()
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC Question 5
 # MAGIC Let’s look at this a different way. What is the average number of calories in each Beverage_prep type?
+
+# COMMAND ----------
+
+coffee_lower.groupBy("beverage_prep").agg(mean("calories")).sort("avg(calories)", ascending = False).show()
 
 # COMMAND ----------
 
@@ -66,6 +89,10 @@ coffee_subset.select([F.count(F.when(F.isnan(c) | F.col(c).F.isNull(), c)).alias
 # MAGIC Question 6
 # MAGIC Which Beverage_prep type contains more than the average calories of all drinks?
 # MAGIC Hint: to answer this, you’ll have to first figure out what the average calories across all drinks are, and then use that as a filter for the grouped Beverage_prep data.
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
